614c2caee84d8a2ccc4b87f93ab188a262dfd29e
Nimzyow <n_soufiani@hotmail.com>
2020-06-08 01:02:08 +0100

    passing tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -6,11 +6,16 @@ import { Appointment } from "../src/Appointment";
 describe("Appointment", () => {
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
     const container = document.createElement("div");
-    document.body.appendChild(container);
 
-    ReactDOM.render(component, container);
-    expect(document.body.textContent).toBe("Ashley");
+    ReactDOM.render(<Appointment customer={customer} />, container);
+    expect(container.textContent).toMatch("Ashley");
+  });
+  it("renders another customers first name", () => {
+    const customer = { firstName: "Jordan" };
+    const container = document.createElement("div");
+
+    ReactDOM.render(<Appointment customer={customer} />, container);
+    expect(container.textContent).toMatch("Jordan");
   });
 });
