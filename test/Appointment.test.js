a16ba2e758cc212e8b17bf1da7eabb880d4ba6c6
Nimzyow <n_soufiani@hotmail.com>
2020-06-08 23:59:42 +0100

    passing tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
   let container;
@@ -24,5 +24,20 @@ describe("Appointment", () => {
 
     render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  const render = (component) => ReactDOM.render(component, container);
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
   });
 });
