b6e5f7714148b608d6d62f256fee5761d1fc6587
Nimzyow <n_soufiani@hotmail.com>
2020-06-15 10:26:35 +0100

    tested for list elements in Appointment

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -29,6 +29,11 @@ describe("Appointment", () => {
 
 describe("AppointmentsDayView", () => {
   let container;
+  const today = new Date();
+  const appointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
 
   beforeEach(() => {
     container = document.createElement("div");
@@ -39,5 +44,16 @@ describe("AppointmentsDayView", () => {
   it("renders a div with the right id", () => {
     render(<AppointmentsDayView appointments={[]} />);
     expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+  it("renders multiple appointments in an ol element", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector("ol")).not.toBeNull();
+    expect(container.querySelector("ol").children).toHaveLength(2);
+  });
+  it("renders each appointment in an li", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll("li")).toHaveLength(2);
+    expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
+    expect(container.querySelectorAll("li")[1].textContent).toEqual("13:00");
   });
 });
